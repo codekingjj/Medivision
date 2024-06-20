6ce2183af503c9de0e0d02d479ed2066a5348698
codekingjj <jaejung0413@naver.com>
2024-06-20 12:44:16 +0900

    Init

---
+++ src/main/java/com/medivision/MedivisionApplication.java
@@ -0,0 +1,13 @@
+package com.medivision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedivisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedivisionApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/medivision/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.medivision;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MedivisionApplication.class);
+    }
+
+}
