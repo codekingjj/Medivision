600e95db170283911a41f92422c8dd5ca3474cb2
jujae03 <0719improvement@naver.com>
2024-06-20 16:23:00 +0900

    Create AdminRepository

--- src/main/java/com/medivision/medivision/user/domain/AdminRepository.java
+++ src/main/java/com/medivision/medivision/user/domain/AdminRepository.java
@@ -1,8 +1,8 @@
-package com.medivision.user.domain;
+package com.medivision.medivision.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository extends JpaRepository<AdminEntity,Integer> {
+public interface AdminRepository extends JpaRepository<AdminEntity, Integer> {
 }
