64390784e88861fa5dcacf2d52bf647e2e2e6144
jujae03 <0719improvement@naver.com>
2024-06-20 16:22:49 +0900

    Modify MedivisionDataSourceConfig: basepackage 변경

---
+++ src/main/java/com/medivision/config/MedivisionDataSourceConfig.java
@@ -0,0 +1,71 @@
+package com.medivision.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.medivision.medivision",
+        entityManagerFactoryRef = "testEntityManager",
+        transactionManagerRef = "testTransactionManager"
+)
+@EnableTransactionManagement
+public class MedivisionDataSourceConfig {
+    @Value("${spring.datasource.test.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.test.url}")
+    private String url;
+
+    @Value("${spring.datasource.test.username}")
+    private String username;
+
+    @Value("${spring.datasource.test.password}")
+    private String password;
+
+    @Bean(name = "testDatasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName(driverClassName)
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
+
+    @Primary
+    @Bean(name = "testEntityManager")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.medivision.medivision");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Primary
+    @Bean(name = "testTransactionManager")
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+
+}
