012f1d1738c3d29dd11edad0e7e65515d8d71529
jujae03 <0719improvement@naver.com>
2024-06-20 15:51:34 +0900

    Create SignUpRequestDto

---
+++ src/main/java/com/medivision/user/dto/SignUpRequestDto.java
@@ -0,0 +1,16 @@
+package com.medivision.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+@Getter
+@Setter
+@NoArgsConstructor
+public class SignUpRequestDto {
+
+    @NotBlank
+    private String juminSC;
+    @NotBlank
+    private int userCode;
+}
