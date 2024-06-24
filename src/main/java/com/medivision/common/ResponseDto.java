1bb0cb4121db0d1a464f31d68bd48f6a7067fd59
jujae03 <0719improvement@naver.com>
2024-06-24 11:05:32 +0900

    Renamed ResponseMessage

--- src/main/java/com/medivision/common/ResponseDto.java
+++ src/main/java/com/medivision/common/ResponseDto.java
@@ -13,7 +13,7 @@ public class ResponseDto {
     private String message;
 
     public static ResponseEntity<ResponseDto> databaseError(){
-        ResponseDto responseDto = new ResponseDto(ResponseCode.DATABASE_ERROR,ReponseMessage.DATABASE_ERROR);
+        ResponseDto responseDto = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseDto);
     }
 
