0c6154a6adb857dc2958354d9f339b8649b8158d
jujae03 <0719improvement@naver.com>
2024-06-24 16:09:25 +0900

    ADD JWT LOGIC

--- src/main/java/com/medivision/medivision/jwt/JwtProvider.java
+++ src/main/java/com/medivision/medivision/jwt/JwtProvider.java
@@ -1,4 +1,50 @@
 package com.medivision.medivision.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Component
 public class JwtProvider {
+
+    @Value("${secret-key}")
+    private String secretKey;
+    
+    //jwt 생성 메소드
+    public String create(int userCode){
+
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+
+        String userCodeVarchar = String.valueOf(userCode);
+
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .setSubject(userCodeVarchar).setIssuedAt(new Date()).setExpiration(expiredDate)
+                .compact();
+
+        return jwt;
+    }
+    
+    //jwt 검증 하는 메소드
+    public String validate(String jwt){
+
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parser().setSigningKey(secretKey) //파싱 열고 시크릿키 넣어주고 jwt토큰 넣어준 결과값을 반환
+                    .parseClaimsJws(jwt).getBody();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+        return claims.getSubject();
+    }
+
 }
