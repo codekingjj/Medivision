124a54c09402a8eecee0432e93e03ec7e31d2275
jujae03 <0719improvement@naver.com>
2024-06-24 16:44:56 +0900

    Add UserController: /auth/sign-in Post

--- src/main/java/com/medivision/medivision/user/controller/UserController.java
+++ src/main/java/com/medivision/medivision/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.medivision.medivision.user.controller;
 
 import com.medivision.medivision.user.domain.service.AdminService;
+import com.medivision.medivision.user.domain.service.UserService;
+import com.medivision.medivision.user.dto.request.SignInRequestDto;
 import com.medivision.medivision.user.dto.request.SignUpRequestDto;
 import com.medivision.medivision.user.dto.response.SignInResponseDto;
 import com.medivision.medivision.user.dto.response.SignUpResponseDto;
@@ -14,12 +16,17 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final AdminService adminService;
-
+    private final UserService userService;
     @PostMapping("/admin")
     public ResponseEntity<? super SignUpResponseDto> signup(@RequestBody SignUpRequestDto requestBody){
         ResponseEntity<? super SignUpResponseDto> response = adminService.signUp(requestBody);
         return  response;
     }
 
+    @PostMapping("/auth/sign-in")
+    public ResponseEntity<? super SignInResponseDto> signin(@RequestBody SignInRequestDto requestBody){
+        ResponseEntity<? super  SignInResponseDto> response = userService.signin(requestBody);
+        return response;
+    }
 
 }
