3f946ba4e8560a52bd1c8bbb733877c32c19f183
jujae03 <0719improvement@naver.com>
2024-06-25 17:12:30 +0900

    Move file

--- src/main/java/com/medivision/medivision/jwt/WebConfig.java
+++ src/main/java/com/medivision/medivision/jwt/WebConfig.java
@@ -1,11 +1,15 @@
 package com.medivision.medivision.jwt;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.time.Duration;
+
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
@@ -15,4 +19,9 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
