32fd713df26689b9f48b73ab2a943f499c6fc69b
codekingjj <jaejung0413@naver.com>
2024-06-26 16:59:54 +0900

    Add : tools.js

---
+++ src/main/resources/static/script/viewer/tools.js
@@ -0,0 +1,37 @@
+// import {addTool, MagnifyTool, ToolGroupManager, WindowLevelTool, ZoomTool} from '@cornerstonejs/tools';
+//
+// let cornerstone;
+// cornerstoneTools.external.cornerstone = cornerstone;
+// //
+// addTool(ZoomTool);
+// addTool(WindowLevelTool);
+//
+// const toolGroupId = 'ToolGroup';
+// const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+//
+// toolGroup.addTool(ZoomTool.toolName);
+// toolGroup.addTool(WindowLevelTool.toolName);
+//
+// toolGroup.setToolActive(WindowLevelTool.toolName, {
+//     bindings: [
+//         {
+//             mouseButton: csToolsEnums.MouseBindings.Primary, // Left Click
+//         },
+//     ],
+// });
+//
+// toolGroup.setToolActive(ZoomTool.toolName, {
+//     bindings: [
+//         {
+//             mouseButton: csToolsEnums.MouseBindings.Secondary, // Right Click
+//         },
+//     ],
+// });
+//
+// function magnifyTool() {
+//     const magnifyTool = new MagnifyTool();
+//
+//     cornerstoneTools.setToolActive()
+// }
+//
+//
--- src/main/webapp/WEB-INF/views/viewer/viewer.jsp
+++ src/main/webapp/WEB-INF/views/viewer/viewer.jsp
@@ -1,17 +1,14 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: user
-  Date: 2024-06-24
-  Time: 오전 10:45
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Viewer</title>
-<%--    <script src="resources/static/script/viewer.js"/>--%>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
+<div id="content"></div>
+<input id="file" type="file" accept="application/dicom">
 
 </body>
+<script type="module" src="static/dist/bundle.js"></script>
 </html>
