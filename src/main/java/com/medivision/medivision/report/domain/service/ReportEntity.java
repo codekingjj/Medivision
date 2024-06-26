a4beb1bdafc3f6a3439520b40606b417cad16fb7
da092511 <da092511@naver.com>
2024-06-26 19:22:28 +0900

    Modify constructor

--- src/main/java/com/medivision/medivision/report/domain/service/ReportEntity.java
+++ src/main/java/com/medivision/medivision/report/domain/service/ReportEntity.java
@@ -1,13 +1,18 @@
 package com.medivision.medivision.report.domain.service;
 
+import com.medivision.medivision.report.dto.ReportRequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Getter
 @AllArgsConstructor
@@ -26,6 +31,25 @@ public class ReportEntity {
     private String conclusion;
     private String recommend;
     private Timestamp regDate;
+
     private Timestamp modDate;
     private String typeDecode;
+
+
+    public ReportEntity(ReportRequestDto reportRequestDto) {
+        this.studyKey = reportRequestDto.getStudyKey();
+        this.writer = reportRequestDto.getWriter();
+        this.comment = reportRequestDto.getComment();
+        this.finding = reportRequestDto.getFinding();
+        this.conclusion = reportRequestDto.getConclusion();
+        this.recommend = reportRequestDto.getRecommend();
+        this.typeDecode = reportRequestDto.getTypeDecode();
+        Date date = new Date();
+        System.out.println(date);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Timestamp timestamp = Timestamp.valueOf(dateFormat.format(date));
+        System.out.println(timestamp);
+        this.regDate = timestamp;
+
+    }
 }
