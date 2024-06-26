617a61ddba29b95d691effd5ffc875dd6fa26dc6
dlstjs7617 <dlstjs7617@gmail.com>
2024-06-26 16:57:11 +0900

    create : 상세검색 내용추가

--- src/main/java/com/medivision/medivision/viewer/controller/ViewerController.java
+++ src/main/java/com/medivision/medivision/viewer/controller/ViewerController.java
@@ -1,8 +1,10 @@
 package com.medivision.medivision.viewer.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class ViewerController {
 
     @GetMapping("/viewer")
