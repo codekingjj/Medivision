7e54aeab979d11d5a253b09acaa4c36ce31fc17c
dlstjs7617 <dlstjs7617@gmail.com>
2024-06-26 16:57:07 +0900

    create : 상세검색 내용추가

--- src/main/java/com/medivision/pacs/repository/VStudyRepository.java
+++ src/main/java/com/medivision/pacs/repository/VStudyRepository.java
@@ -4,8 +4,20 @@ import com.medivision.pacs.entity.VStudyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VStudyRepository extends JpaRepository<VStudyEntity, Integer> {
 
 
+
+
+    List<VStudyEntity> findByModality(String modality);
+
+//    되는 코드들
+    List<VStudyEntity> findByPidLike(String pid);
+
+    List<VStudyEntity> findByPnameLike(String pname);
+
+    List<VStudyEntity> findByReportstatus(int reportstatus);
 }
