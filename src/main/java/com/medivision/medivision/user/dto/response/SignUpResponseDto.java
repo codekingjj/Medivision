81e824cff067d662b045132413a4d02cd5722f45
jujae03 <0719improvement@naver.com>
2024-06-27 17:36:27 +0900

    signUpResult.jsp에 userId 출력

--- src/main/java/com/medivision/medivision/user/dto/response/SignUpResponseDto.java
+++ src/main/java/com/medivision/medivision/user/dto/response/SignUpResponseDto.java
@@ -3,16 +3,23 @@ package com.medivision.medivision.user.dto.response;
 import com.medivision.common.ResponseCode;
 import com.medivision.common.ResponseDto;
 import com.medivision.common.ResponseMessage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+@Getter
 public class SignUpResponseDto extends ResponseDto {
-    public SignUpResponseDto() {
+
+    private String userId;
+    public SignUpResponseDto(String userId) {
         super(ResponseCode.SUCCESS, ResponseMessage.SUCCES);
+        this.userId=userId;
     }
 
-    public static ResponseEntity<SignUpResponseDto> success(){
-        SignUpResponseDto result = new SignUpResponseDto();
+    public static ResponseEntity<SignUpResponseDto> success(String userId){
+        SignUpResponseDto result = new SignUpResponseDto(userId);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
