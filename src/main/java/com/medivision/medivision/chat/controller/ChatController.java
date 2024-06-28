eb1ab35e23ef5f0be26208ab236f832bea515d59
typoscript <minkyu.kim.contact@gmail.com>
2024-06-28 17:30:10 +0900

    Modify ChatController

--- src/main/java/com/medivision/medivision/chat/controller/ChatController.java
+++ src/main/java/com/medivision/medivision/chat/controller/ChatController.java
@@ -1,16 +1,21 @@
 package com.medivision.medivision.chat.controller;
 
+import com.medivision.medivision.chat.dto.ChatRequestDto;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatController {
-    @MessageMapping("/sendMessage")
-    @SendTo("/chat/messages")
-    public String sendMessage(String message) throws Exception {
-        System.out.println("message:  " + message);
-        return message;
+    @MessageMapping("/chat/sendMessage")
+    @SendTo("/topic/messages")
+    public String sendMessage(@RequestBody ChatRequestDto chatRequestDto) throws Exception {
+        System.out.println("room id:  " + chatRequestDto.getRoomId());
+        System.out.println("sender user code:  " + chatRequestDto.getSenderUserCode());
+        System.out.println("message:  " + chatRequestDto.getMessage());
+
+        return chatRequestDto.getMessage();
     }
 }
