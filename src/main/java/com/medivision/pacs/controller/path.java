ab3b870bffc67a92594d7512a422aa0578435832
dlstjs7617 <dlstjs7617@gmail.com>
2024-07-01 18:06:38 +0900

    create 파일 패스 추가

---
+++ src/main/java/com/medivision/pacs/controller/path.java
@@ -0,0 +1,13 @@
+package com.medivision.pacs.controller;
+
+public class path {
+
+
+    public String getPath(String studyKey){
+        String realPath = "";
+
+        String path = "";
+
+        return  realPath;
+    }
+}
