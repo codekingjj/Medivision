2db83265272aaf7457e34cdad6ca690de77475e5
da092511 <da092511@naver.com>
2024-07-02 16:38:53 +0900

    Add antMatchers url

--- src/main/java/com/medivision/medivision/jwt/WebSecurityConfig.java
+++ src/main/java/com/medivision/medivision/jwt/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig {
                 .antMatchers("/","/main/**","/admin/**","/auth/**","/viewer/**","/test","/test232","/search/**","/style/**","/assets/**", "/js/**",
                         "/static/**",
                         "/script/**","/reportPage","/patientBookmark/**",
-                        "/report/{**}","/reports/{**}","/report/targetReport","/report")
+                        "/report/{**}","/reports/{**}","/report/targetReport**","/report")
                 .permitAll() //매칭에 따라서 작업 여부
                 .antMatchers(HttpMethod.GET,"/board/**").permitAll()
                 .anyRequest().authenticated().and()
