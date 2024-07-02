a4c8c27d194f7c1e3f05a7249812bf330d0f0160
dlstjs7617 <dlstjs7617@gmail.com>
2024-07-02 18:04:15 +0900

    create : dicomPath 추가

--- src/main/java/com/medivision/pacs/controller/path.java
+++ src/main/java/com/medivision/pacs/controller/path.java
@@ -1,13 +1,27 @@
 package com.medivision.pacs.controller;
 
+import com.medivision.pacs.entity.VImageEntity;
+import com.medivision.pacs.service.VImageService;
+
+import java.io.File;
+import java.util.List;
+
 public class path {
 
+    VImageService vImageService;
 
-    public String getPath(String studyKey){
+    public String getPath(int studyKey){
         String realPath = "";
 
-        String path = "";
+        String path;
 
         return  realPath;
+    }
+
+
+    public File getFile(String path){
+        File file = new File(path);
+        return file;
+
     }
 }
