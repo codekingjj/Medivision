acdfe817df989604facc45e9977e64788fec9100
codekingjj <jaejung0413@naver.com>
2024-07-03 17:25:28 +0900

    Modify : ViewerController

--- src/main/java/com/medivision/medivision/viewer/domain/service/ViewerService.java
+++ src/main/java/com/medivision/medivision/viewer/domain/service/ViewerService.java
@@ -1,11 +1,19 @@
 package com.medivision.medivision.viewer.domain.service;
 
+import com.medivision.pacs.entity.VSeriesEntity;
+import com.medivision.pacs.repository.VSeriesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ViewerService {
+    private final VSeriesRepository vSeriesRepository;
 
+    public List<VSeriesEntity> findSeriesByStudyKey(int studyKey) {
+        return vSeriesRepository.findByStudyKey(studyKey);
+    }
 
 }
