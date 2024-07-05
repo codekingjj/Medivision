c0b4b87f5adf39e2e051a677cf2846123261402d
typoscript <>
2024-07-05 17:42:04 +0900

    Modify AdminRepository

--- src/main/java/com/medivision/medivision/user/domain/repository/AdminRepository.java
+++ src/main/java/com/medivision/medivision/user/domain/repository/AdminRepository.java
@@ -1,13 +1,21 @@
 package com.medivision.medivision.user.domain.repository;
 
 import com.medivision.medivision.user.domain.entity.AdminEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AdminRepository extends JpaRepository<AdminEntity, Integer> {
 
      AdminEntity findByUserCode(int userCode);
 
+     @Query(nativeQuery = true, value = "SELECT a.* FROM chat_room_and_members c JOIN admin a ON c.user_code = a.user_code WHERE c.room_id = ?1")
+     List<AdminEntity> findAllByRoomId(int roomId);
 
+     Page<AdminEntity> findAllByUserNameContaining(Pageable pageable, String userName);
 }
