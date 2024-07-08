f4e4bea6b3758c98d0a125164c363729cccc1929
typoscript <>
2024-07-08 17:07:57 +0900

    Modify ChatUserSearch Files

--- src/main/resources/static/script/chat/chatUserSearch/UserElement.js
+++ src/main/resources/static/script/chat/chatUserSearch/UserElement.js
@@ -1,29 +1,28 @@
 class UserElement {
-    static getUsers(users) {
+    static getUsers(loggedInUserCode, users) {
         const userContainerList = users.map(user => {
-            return this.#create(user);
+            return this.#create(loggedInUserCode, user);
         });
 
         return userContainerList;
     }
 
-    static #createUserContainer(userCode) {
+    static #createUserContainer(loggedInUserCode, userCode) {
         const userContainer = document.createElement("div");
 
         userContainer.classList.add("user-container");
 
         userContainer.addEventListener("click", () => {
-            //location.href = `/chatroom/${userCode}`;
-            console.log(userCode);
+            this.#addUsersToChatroom(loggedInUserCode, userCode);
         });
 
         return userContainer;
     }
 
-    static #create(user) {
+    static #create(loggedInUserCode, user) {
         const { userCode, userName } = user;
 
-        const userContainer = this.#createUserContainer(userCode);
+        const userContainer = this.#createUserContainer(loggedInUserCode, userCode);
 
         const content = document.createElement("span");
 
@@ -32,6 +31,36 @@ class UserElement {
         userContainer.append(content);
 
         return userContainer;
+    }
+
+    static #addUsersToChatroom(loggedInUserCode, userCode) {
+        console.log("my userCode: " + loggedInUserCode);
+        console.log("adding userCode: " + userCode);
+
+        const bodyData = {
+            name: loggedInUserCode + ", " +userCode,
+            creatorUserCode: loggedInUserCode,
+            members: [
+                { userCode: loggedInUserCode },
+                { userCode: userCode },
+            ],
+        };
+
+        fetch("/chatroomAndMember/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+            },
+            body: JSON.stringify(bodyData),
+        })
+            .then(response => {
+                if (response.ok)
+                    window.location.href = "/chatroom";
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
 }
 
