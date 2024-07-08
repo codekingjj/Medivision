27a89c0cac42dbc7eb85e46f9cdd8b31cddb57c5
typoscript <>
2024-07-08 17:07:15 +0900

    Modify ChatroomMember Files

--- src/main/java/com/medivision/medivision/chat/chatroomMember/domain/ChatroomMemberId.java
+++ src/main/java/com/medivision/medivision/chat/chatroomMember/domain/ChatroomMemberId.java
@@ -13,17 +13,4 @@ import java.util.Objects;
 public class ChatroomMemberId implements Serializable {
     private int roomId;
     private int userCode;
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(roomId, userCode);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ChatroomMemberId chatroomMemberId = (ChatroomMemberId) o;
-        return roomId == chatroomMemberId.roomId && userCode == chatroomMemberId.userCode;
-    }
 }
