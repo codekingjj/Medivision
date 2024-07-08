7b41c5c4ffe6f4ee51fef22e4be13b3f7b742ebc
typoscript <>
2024-07-08 17:04:11 +0900

    Add Chat JS Files

---
+++ src/main/resources/static/script/chat/ChatPage.js
@@ -0,0 +1,18 @@
+class ChatPage {
+    static pops = [];
+
+    static popup() {
+        var url = `/chatroom`;
+        var name = "targetReport";
+        var option = "width=800, height=500, left=100, top=50, location=no";
+
+        const pop = window.open(url, name, option);
+        this.pops.add(pop);
+    }
+}
+
+window.onload = () => {
+   $("#btnChatOpen").on("click", () => {
+       ChatPage.popup();
+   });
+}
