0b8d929cc096ba4ed5038f7ec8767e3e86c0483b
jujae03 <0719improvement@naver.com>
2024-07-11 14:19:36 +0900

    report read시 log 찍기

--- src/main/java/com/medivision/medivision/log/study/controller/StudyLogController.java
+++ src/main/java/com/medivision/medivision/log/study/controller/StudyLogController.java
@@ -1,5 +1,7 @@
 package com.medivision.medivision.log.study.controller;
 
+import com.medivision.medivision.log.study.domain.service.StudyLogService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -7,15 +9,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLOutput;
 
 @Controller
+@RequiredArgsConstructor
 public class StudyLogController {
 
+    private final StudyLogService studyLogService;
+
     @GetMapping("/log/{studyKey}")
-    public void studyKey(@PathVariable("studyKey") String studyKey, @AuthenticationPrincipal String userCode){
-        System.out.println("여기");
-        System.out.println("key:" + studyKey);
-        System.out.println("userCode"+userCode);
+    public void studyKey(@PathVariable("studyKey") String studyKey, @AuthenticationPrincipal String userCode, HttpServletRequest request){
+        String ip = request.getRemoteAddr();
+        studyLogService.saveStudyLog(userCode, studyKey, ip);
     }
 }
