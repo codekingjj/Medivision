14c30ea86ca3f353be2cc546e2557296e8925d33
codekingjj <jaejung0413@naver.com>
2024-07-14 17:14:56 +0900

    Modify : viewer.js

--- src/main/resources/static/script/viewer/tools.js
+++ src/main/resources/static/script/viewer/tools.js
@@ -1,10 +1,8 @@
 import * as cornerstoneTools from "@cornerstonejs/tools";
-
 const {
     //1. 툴선언
     //도구 -> 돋보기
     MagnifyTool,
-    TrackballRotateTool,
     //도구 -> 확대, 축소
     ZoomTool,
     ToolGroupManager,
@@ -22,7 +20,8 @@ const {
     PlanarFreehandContourSegmentationTool,
     BidirectionalTool,
     CobbAngleTool,
-    EraserTool
+    EraserTool,
+    PlanarRotateTool,
 } = cornerstoneTools;
 
 const { MouseBindings } = csToolsEnums;
@@ -34,7 +33,6 @@ const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
 const content = document.getElementsByClassName('content');
 const moveBtn = document.getElementById('defaultTool');
 const windowBtn = document.getElementById('windowLevel');
-// const invertBtn = document.getElementById('invert');
 const angleBtn = document.getElementById('activateAngle');
 const arrowBtn = document.getElementById('activateArrowAnnotate');
 const probeBtn = document.getElementById('activateProbe');
@@ -45,11 +43,12 @@ const freeHandBtn = document.getElementById('activateFreeHand');
 const bidirectionBtn = document.getElementById('activateBidirectional');
 const cobbangleBtn = document.getElementById('activateCobbAngle');
 const eraserBtn = document.getElementById('activateEraser');
-// let annotationBox = document.getElementById('annotationBox');
+const zoomBtn = document.getElementById('activateZoom');
+const rotateBtn = document.getElementById('activateRotate');
+const rRotateBtn = document.getElementById('rRotate');
 //3. 버튼체크
 let isPanToolActive = false;
 let isWindowActive = false;
-// let isInvertActive;
 let isAngleToolActive = false;
 let isArrowToolActive = false;
 let isProbeToolActive = false;
@@ -60,82 +59,137 @@ let isFreeHandToolActive = false;
 let isBidirectionToolActive = false;
 let isCobbAngleToolActive = false;
 let isEraserToolActive =false;
-// let annotationDisplay = false;
-
-
-// let selectedDivById = "";
-
+let isZoomToolActive = false;
+let isRotateToolActive = false;
 
 //4. 버튼별 함수 지정
 moveBtn.addEventListener('click', function() {
-    cornerstoneTools.init();
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'Pan') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     movement_pan();
 })
 windowBtn.addEventListener('click', function() {
-    cornerstoneTools.init();
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'WindowLevel') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     windowLevel();
 })
-// invertBtn.addEventListener('click', function () {
-//     if (selectedDivById.getAttribute('invert') === 'unchecked') {
-//         selectedDivById.setAttribute('invert', 'checked');
-//         isInvertActive = true;
-//     } else {
-//         selectedDivById.setAttribute('invert', 'unchecked');
-//         isInvertActive = false;
-//     }
-//     invertImageWithWWWC(selectedDivById);
-// });
+
 angleBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'Angle') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateAngle();
 })
 arrowBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'ArrowAnnotate') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateArrow();
 })
 probeBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'Probe') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateProbe();
 })
 lengthBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'Length') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateLength();
 })
 rectangleBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'RectangleROI') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateRectangle();
 })
 ellipticalBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'EllipticalROI') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateElliptical();
 })
 freeHandBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'PlanarFreehandContourSegmentationTool') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateFreeHand();
 })
 bidirectionBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'Bidirectional') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateBidirection();
 })
 cobbangleBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'CobbAngle') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateCobbAngle();
 })
 eraserBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'Eraser') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
     activateEraser();
 })
-
-// function showAnnotationBox() {
-//     if(!annotationDisplay) {
-//         annotationBox.style.display='none'
-//     }else {
-//         annotationBox.style.display = 'inline-block';
-//     }
-//     annotationDisplay = !annotationDisplay;
-// }
-// window.addEventListener('click', function (e) {
-//     if(e.target.id !== 'annotation')
-//         annotationBox.style.display = 'none';
-//     else {
-//         showAnnotationBox();
-//     }
-// })
-
-
-
-
-
+zoomBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'Zoom') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
+    activateZoom()
+})
+rotateBtn.addEventListener('click', function() {
+    const setActive = toolGroup.getActivePrimaryMouseButtonTool();
+    if (setActive === 'PlanarRotate') {
+        return;
+    }else {
+        toolDisabled(setActive);
+    }
+    activateRotate();
+})
 
 function toolMaker() {
     cornerstoneTools.init();
@@ -154,6 +208,8 @@ function toolMaker() {
     cornerstoneTools.addTool(BidirectionalTool);
     cornerstoneTools.addTool(CobbAngleTool);
     cornerstoneTools.addTool(EraserTool);
+    cornerstoneTools.addTool(ZoomTool);
+    cornerstoneTools.addTool(PlanarRotateTool);
     //6. 툴그룹에 add
     toolGroup.addTool(PanTool.toolName, {cursor:'move'});
     toolGroup.addTool(WindowLevelTool.toolName, {cursor:'window'});
@@ -169,35 +225,11 @@ function toolMaker() {
     toolGroup.addTool(BidirectionalTool.toolName, {cursor:'bidirectional'});
     toolGroup.addTool(CobbAngleTool.toolName, {cursor:'cobbAngle'});
     toolGroup.addTool(EraserTool.toolName, {cursor:'eraser'});
-    // toolGroup.addTool(Invert)
+    toolGroup.addTool(ZoomTool.toolName, {cursor:'zoom'});
+    toolGroup.addTool(PlanarRotateTool.toolName, {cursor:'rotate'});
 }
 
 toolMaker();
-
-
-
-
-
-
-// cornerstoneTools.addTool(PanTool);
-// toolGroup.addTool(PanTool.toolName);
-
-
-// $(".dicomImage").on("click", e => {
-//     console.log("clicked")
-//     const id = e.target;
-//     console.log(id)
-//     // console.log("engine id: " + id[0]);
-//     // console.log("viewport id: " + id[1]);
-//     //
-//     toolGroup.addViewport(id[1], id[0]);
-// })
-
-// for (let i = 0; i < viewportIds.length; i++) {
-//     const viewportId = viewportIds[i];
-//     const renderingEngineId = renderingEngineIds[i];
-//     toolGroup.addViewport(viewportId, renderingEngineId);
-// }
 
 
 document.getElementById("workList").addEventListener("click", function () {
@@ -206,22 +238,64 @@ document.getElementById("workList").addEventListener("click", function () {
 
 //7. 함수 만들기
 
+function toolDisabled(setActive) {
+    switch (setActive) {
+        case 'Pan' :
+            movement_pan();
+            break;
+        case 'WindowLevel' :
+            windowLevel();
+            break;
+        case 'Angle' :
+            activateAngle();
+            break;
+        case 'ArrowAnnotate' :
+            activateAngle();
+            break;
+        case 'Probe' :
+            activateProbe();
+            break;
+        case 'Length' :
+            activateLength();
+            break;
+        case 'RectangleROI':
+            activateRectangle();
+            break;
+        case 'EllipticalROI':
+            activateElliptical();
+            break;
+        case 'PlanarFreehandContourSegmentationTool':
+            activateFreeHand();
+            break;
+        case 'Bidirectional':
+            activateBidirection();
+            break;
+        case 'CobbAngle' :
+            activateCobbAngle();
+            break;
+        case 'Eraser' :
+            activateEraser();
+            break;
+        case 'Zoom':
+            activateZoom();
+            break;
+        case 'PlanarRotate':
+            activateRotate();
+            break;
+    }
+}
+
 function movement_pan() {
-    // const PanTool = cornerstoneTools.PanTool;
     if(isPanToolActive) {
         moveBtn.style.backgroundColor = 'white';
         toolGroup.setToolDisabled('Pan');
         toolGroup.setToolDisabled('StackScrollMouseWheel');
         toolGroup.setToolDisabled('Magnify')
-        // cornerstoneTools.setToolDisabled('Pan');
     }else {
         moveBtn.style.backgroundColor = '#9b9b9b';
-        // cornerstoneTools.addTool(PanTool);
         toolGroup.setToolActive('Pan', {bindings: [{mouseButton: csToolsEnums.MouseBindings.Primary}]});
         toolGroup.setToolActive('StackScrollMouseWheel', {bindings:[{mouseButton:csToolsEnums.MouseBindings.Auxiliary}]})
         toolGroup.setToolActive('Magnify', {bindings:[{mouseButton:csToolsEnums.MouseBindings.Secondary}]})
-        // toolGroup.setToolActive('StackScroll', {bindings:[{mouseButton:csToolsEnums.MouseBindings.Secondary}]})
-        // cornerstoneTools.setToolActive('Pan', {mouseButtonMask: 1})
     }
     isPanToolActive = !isPanToolActive;
 }
@@ -237,19 +311,6 @@ function windowLevel() {
     isWindowActive = !isWindowActive;
 }
 
-// function invertImageWithWWWC(divById) {
-//     const selectedDiv = cornerstone.getEnabledElement(divById).element;
-//     const viewport = cornerstone.getViewport(selectedDiv);
-//     viewport.invert = invertCheck;
-//     cornerstone.setViewport(selectedDiv, viewport);
-// }
-//
-// function invertHandler(divById) {
-//     selectedDivById = divById;
-//     const invertVal = divById.getAttribute('invert');
-//     if (invertVal === null)
-//         divById.setAttribute('invert', 'unchecked'); // checked
-// }
 
 function activateAngle() {
     if(isAngleToolActive) {
@@ -355,76 +416,36 @@ function activateEraser() {
     }
     isEraserToolActive = !isEraserToolActive;
 }
+function activateZoom() {
+    if(isZoomToolActive) {
+        zoomBtn.style.backgroundColor = 'white';
+        toolGroup.setToolDisabled('Zoom');
+    }else {
+        zoomBtn.style.backgroundColor='#9b9b9b';
+        toolGroup.setToolActive('Zoom', {bindings: [{mouseButton: csToolsEnums.MouseBindings.Primary}]});
+    }
+    isZoomToolActive = !isZoomToolActive;
+}
 
-
-//
-// const init = async () => {
-//     await cornerstone.init();
-//     cornerstoneDICOMImageLoader.external.cornerstone = cornerstone;
-//     cornerstoneDICOMImageLoader.external.dicomParser = dicomParser;
-//
-//     var config = {
-//         maxWebWorkers: navigator.hardwareConcurrency || 1,
-//         startWebWorkersOnDemand: true,
-//         taskConfiguration: {
-//             decodeTask: {
-//                 initializeCodecsOnStartup: false,
-//             },
-//             sleepTask: {
-//                 sleepTime: 3000,
-//             },
-//         },
-//     };
-//
-//     cornerstoneDICOMImageLoader.webWorkerManager.initialize(config);
-// };
-//
-//
-//
-// // cornerstone3DTools.addTool(ZoomTool);
-// // cornerstone3DTools.addTool(WindowLevelTool);
-// //
-// // const toolGroupId = 'ToolGroup';
-// // const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
-// //
-// //
-// // toolGroup.addTool(ZoomTool.toolName);
-// // toolGroup.addTool(WindowLevelTool.toolName);
-// //
-// // toolGroup.setToolActive(WindowLevelTool.toolName, {
-// //     bindings: [
-// //         {
-// //             mouseButton: csToolsEnums.MouseBindings.Primary, // Left Click
-// //         },
-// //     ],
-// // });
-// //
-// // toolGroup.setToolActive(ZoomTool.toolName, {
-// //     bindings: [
-// //         {
-// //             mouseButton: csToolsEnums.MouseBindings.Secondary, // Right Click
-// //         },
-// //     ],
-// // });
-//
+function activateRotate() {
+    if (isRotateToolActive) {
+        rotateBtn.style.backgroundColor = 'white';
+        toolGroup.setToolDisabled('PlanarRotate');
+    }else {
+        rotateBtn.style.backgroundColor='#9b9b9b';
+        toolGroup.setToolActive('PlanarRotate', {bindings: [{mouseButton: csToolsEnums.MouseBindings.Primary}]});
+    }
+    isRotateToolActive = !isRotateToolActive;
+}
 
 export const setTools = (viewportId, renderingEngineId) => {
-    // cornerstoneTools.init();
-    //
-    // cornerstoneTools.addTool(PanTool.toolName);
-    //
-    // toolGroup.addTool(PanTool.toolName, {cursor:'move'});
-
     toolGroup.addViewport(viewportId, renderingEngineId);
 }
 movement_pan();
 
-// function handleContextMenu(event) {
-//     if (event.target === content) {
-//         event.preventDefault();
-//     }
-// }
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
 });
+
+
 
