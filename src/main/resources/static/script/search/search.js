053195195c5d849d57423d98fae3d6c9924fb7fc
dlstjs7617 <dlstjs7617@gmail.com>
2024-07-15 17:04:50 +0900

    add : footer추가

--- src/main/resources/static/script/search/search.js
+++ src/main/resources/static/script/search/search.js
@@ -86,33 +86,6 @@ $(document).ready(function() {
 
                 pageCntSetting();
             },
-
-
-            //     let count = '<p>' + data.length + '명의 환자를 찾았습니다.</p>';
-            //     countArea.append(count);
-            //     data.forEach(function(item) {
-            //         if(item.reportstatus === 3){
-            //             item.reportstatus = "읽지않음";
-            //         }else if(item.reportstatus === 5){
-            //             item.reportstatus = "예비판독";
-            //         }else if(item.reportstatus === 6){
-            //             item.reportstatus = "판독";
-            //         }
-            //         var row = '<tr id=' + item.studyKey + ' class="tr-area" >' +
-            //             // 담당 환자 추가를 위한 체크박스
-            //             '<td class="td-patientBookmark-checkbox-container">' + `<input type="checkbox" class="checkbox-patientBookmark" id=${item.pid} />` + '</td>' +
-            //             '<td>' + item.pid + '</td>' +
-            //             '<td>' + item.pname + '</td>' +
-            //             '<td>' + item.modality + '</td>' +
-            //             '<td>' + item.studydesc + '</td>' +
-            //             '<td>' + item.studydate + '</td>' +
-            //             '<td>' + item.reportstatus + '</td>' +
-            //             '<td>' + item.seriescnt + '</td>' +
-            //             '<td>' + item.imagecnt + '</td>' +
-            //             '</tr>';
-            //         tbody.append(row);
-            //     });
-            // },
             error: function(error) {
                 console.error('Error fetching data', error);
             }
@@ -154,8 +127,8 @@ $(document).ready(function() {
         const render = (imageId, index) => {
             // 새로운 div 요소 생성
             const element = document.createElement('div');
-            element.style.width = '150px';
-            element.style.height = '150px';
+            element.style.width = '250px';
+            element.style.height = '250px';
             element.style.marginBottom = '10px';
             element.id = `dicomImage${index}`;
 
